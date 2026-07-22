f5005c57d85d6053e6710df260c5099efa8e9493
Vishnu-creator-cpu <v2343190@gmail.com>
2026-07-22 08:20:13 +0530

    Update UI design, glassmorphism components, and frontend styling

--- smart-complaint-frontend/src/index.js
+++ smart-complaint-frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./index.css";
 
 import "./styles/Variables.css";
+import "./styles/Animations.css";
+import "./styles/Glass.css";
+import "./styles/Global.css";
+import "./index.css";
+
 import App from './App';
-import './styles/Global.css';
 import AuthProvider from "./context/AuthContext";
+
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'bootstrap/dist/js/bootstrap.bundle.min.js';
